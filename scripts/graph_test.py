5c1d2f2726f864d9a910364cfda3585ea293e613
BizCompliancer <mahesh.sharma2@protonail.com>
2022-06-08 19:46:04 +0530

    added 3 vertex and 4 edges more for testing.

--- scripts/graph_test.py
+++ scripts/graph_test.py
@@ -11,13 +11,21 @@ graph = Graph()
 #creating vertex for graph
 node1 = Vertex("Node1")
 node2 = Vertex("Node2")
-node3 = Vertex("Node2")
+node3 = Vertex("Node3")
+node4 = Vertex("Node4")
+node5 = Vertex("Node5")
+node6 = Vertex("Node6")
 
 # starting edge and ending edge
 
 node1_node2 = Edge(node1, node2)
 node1_node3 = Edge(node1, node3)
 node2_node3 = Edge(node2, node3)
+node3_node4 = Edge(node3, node4)
+node3_node5 = Edge(node3, node5)
+node4_node5 = Edge(node4, node5)
+node5_node6 = Edge(node5, node6)
+
 
 
 # adding vertex to graph
@@ -25,12 +33,21 @@ node2_node3 = Edge(node2, node3)
 graph.add_vertex(node1)
 graph.add_vertex(node2)
 graph.add_vertex(node3)
+graph.add_vertex(node4)
+graph.add_vertex(node5)
+graph.add_vertex(node6)
+
 
 # Creating edges to join all the vertex
 
 graph.add_edge(node1, node2, node1_node2)
 graph.add_edge(node1, node3, node1_node3)
 graph.add_edge(node2, node3, node2_node3)
+graph.add_edge(node3, node4, node3_node4)
+graph.add_edge(node3, node5, node3_node5)
+graph.add_edge(node4, node5, node4_node5)
+graph.add_edge(node5, node6, node5_node6)
+
 
 # print Graph
 
