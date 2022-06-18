4eb71acc7ee78d8a52f49c2fb2cd7d72d3458095
BizCompliancer <mahesh.sharma2@protonmail.com>
2022-06-18 03:42:49 -0700

    updated GoogleDrive.py

--- scripts/GoogleDrive.py
+++ scripts/GoogleDrive.py
@@ -9,14 +9,23 @@ folder = '1S_b2h-CV8eiyZAUgDTwmNar_BptkcOkx' # Google drive folder paath
 
 directory = "D:/New folder/BizCompliancer/data"     # Directory path of which we want to upload the file
 
-for f in os.listdir(directory):             # os.listdir() is used to iterate the list of file in the directory of which path is provided in directory variable
-    file_path = os.path.join(directory, f)      # os.path.join(folder_path, file_name) - this function is used to merge the path and file.
-    print(file_path)
-    print(f)
-    gfile = drive.CreateFile({'parents': [{'id': folder}], 'title': f})         #drive.CreateFile() - used to create a file for Google Drive.
+# for f in os.listdir(directory):             # os.listdir() is used to iterate the list of file in the directory of which path is provided in directory variable
+#     file_path = os.path.join(directory, f)      # os.path.join(folder_path, file_name) - this function is used to merge the path and file.
+#     print(file_path)
+#     print(f)
+#     gfile = drive.CreateFile({'parents': [{'id': folder}], 'title': f})         #drive.CreateFile() - used to create a file for Google Drive.
+#
+#     gfile.SetContentFile(file_path)  # SetContentFile() - Used to set file.
+#     gfile.Upload()          # .Upload - used to upload file.
 
-    gfile.SetContentFile(file_path)  # SetContentFile() - Used to set file.
-    gfile.Upload()          # .Upload - used to upload file.
+
+file_list = drive.ListFile({'q': f"'{folder}' in parents and trashed  = false"}).GetList()      # doubt
+
+for index, file in enumerate(file_list):        # doubt in enumerate()
+    file.GetContentFile(file['title'])          # to download the file
+
+    print(index+1, 'File downloaded: ', file['title'])
+
 
 
 
