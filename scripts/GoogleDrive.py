c4e6c65dc8540cc8e6224c2b9bb695288d03ce50
BizCompliancer <mahesh.sharma2@protonmail.com>
2022-06-18 01:59:31 -0700

    added GoogleDrive.py

---
+++ scripts/GoogleDrive.py
@@ -0,0 +1,22 @@
+import os
+from pydrive.auth import GoogleAuth #pydrive help us to download and upload files to google drive.
+from  pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+drive = GoogleDrive(gauth)
+
+folder = '1S_b2h-CV8eiyZAUgDTwmNar_BptkcOkx' # Google drive folder paath
+
+directory = "D:/New folder/BizCompliancer/data"     # Directory path of which we want to upload the file
+
+for f in os.listdir(directory):             # os.listdir() is used to iterate the list of file in the directory of which path is provided in directory variable
+    file_path = os.path.join(directory, f)      # os.path.join(folder_path, file_name) - this function is used to merge the path and file.
+    print(file_path)
+    print(f)
+    gfile = drive.CreateFile({'parents': [{'id': folder}], 'title': f})         #drive.CreateFile() - used to create a file for Google Drive.
+
+    gfile.SetContentFile(file_path)  # SetContentFile() - Used to set file.
+    gfile.Upload()          # .Upload - used to upload file.
+
+
+
