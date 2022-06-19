6bac0cad5f2a5b557df4a5d9ca36a595159c88f4
BizCompliancer <mahesh.sharma2@protonmail.com>
2022-06-19 04:58:39 -0700

    UPDATED Dropbox.py

--- Dropbox/Dropbox.py
+++ Dropbox/Dropbox.py
@@ -8,20 +8,38 @@ class Dropbox:
 
 
     def check_auth(self):
-        current_acc = self.dbx.users_get_current_account()
-        print(current_acc)
+        try:
+            current_acc = self.dbx.users_get_current_account()
+            print(current_acc)
+            print("Connected to Dropbox successfully ")
+        except Exception as e:
+            print(str(e))
 
     def upload_files(self):
-        file_from = r"D:/New folder/BizCompliancer/Dropbox/data/1.cpp"
-        file_to = "/home/Apps/BizC/Test.cpp"
-        with open(file_from, 'rb') as file:
-            self.dbx.files_upload(file.read(), file_to)
-        print('Your file uploaded successfully')
+        try:
+            file_from = r"D:/New folder/BizCompliancer/Dropbox/data/1.cpp"
+            file_to = "/home/Apps/BizC/Test.cpp"
+            with open(file_from, 'rb') as file:
+                self.dbx.files_upload(file.read(), file_to)
+            print('Your file uploaded successfully')
+        except Exception as e:
+            print("Error while uploading file to Dropbox :  " + str(e))
+
+    def download_files(self):
+        try:
+            local_file_path = r"C:\Users\win10\Downloads"
+            with open(local_file_path, "wb") as file:
+                metadata, result = self.dbx_files_download(path= "/home/Apps/BizC/home/Apps/BizC/test.cpp")
+                file.write(result.content)
+        except Exception as e:
+            print("File downloading failed" + str(e))
 
 
 
-at = 'sl.BJwsQDbdSKNsM3nsMdGQrnWyLaUR24R3Iy4ky_Mn-Yc6OM86l86ozq5r2brfR0LqzeEENUaLpCmfQI7RUOlHu2sY5g3sORW8ahhlZGzeogVkqXDNAU-NtSg-pCsA0yldoPkenXk'
+
+at = 'sl.BJ1g8CPCRgCgddsk-W3FSBcrI8KqC4Dt4bbjKJi4q661JH-VzYNKFJkHtT6LazZAQQZE2gC19_VsuAtjEhyZtkCgPi7CK3Tu729Q8_0zS6vW24c88WSeEIo_ccu7ErUofLEoECH5RIM'
 user = Dropbox(at)
 user.check_auth()
-user.upload_files()
+#user.upload_files()
+user.download_files()
 
